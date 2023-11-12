b61853c0227fdb605e352262d78b257ab896c970
Salavat Mukhamedzhanov <salavat.mukhamedzhanov@privacy-solutions.org>
2023-11-12 19:04:43 +0100

    feat: city card component

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from '../../model/city.model';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CityStore } from '../../data-access/city.store';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card
+      [list]="cities"
+      [addItemCallback]="addNewItem"
+      [deleteItemCallback]="deleteItem"
+      [getName]="getName"
+      customClass="bg-light-blue">
+      <img src="assets/img/city.png" width="200px" />
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: City[] = [];
 
-  ngOnInit(): void {}
+  constructor(private http: FakeHttpService, private store: CityStore) {}
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+
+    this.store.cities$.subscribe((c) => (this.cities = c));
+  }
+
+  protected addNewItem = () => this.store.addOne(randomCity());
+
+  protected deleteItem = (id: number) => this.store.deleteOne(id);
+
+  protected getName = (item: City) => item.name;
 }
